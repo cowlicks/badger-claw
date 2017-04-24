4edd310c6e6dcdb32860952dbb68876831e4e8b7
Blake Griffith <cowlicks@riseup.net>
2017-04-23 17:13:41 -0700

    Add virtualenvwrapper to os requirements.

--- setup-env.sh
+++ setup-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install chromium-browser chromium-chromedriver python-virtualenv xvfb
+sudo apt-get install -y chromium-browser chromium-chromedriver python-virtualenv xvfb virtualenvwrapper
 mkvirtualenv crawler
 pip install -r requirements.txt
