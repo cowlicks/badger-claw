cd2fa61d17a85498b113d666cac8768e52f2d459
Blake Griffith <cowlicks@riseup.net>
2017-04-23 20:25:52 -0700

    Add an index updating script.

---
+++ update_index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+import json
+
+new_item = sys.argv[1]
+
+index_location = 'results/index.json'
+with open(index_location, 'r') as f:
+    index = json.load(f)
+
+index['index'].insert(0, new_item)
+
+with open(index_location, 'w') as f:
+    f.write(json.dumps(index, indent=4, sort_keys=True))
