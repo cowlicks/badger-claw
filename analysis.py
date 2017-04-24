95f8cc4c29a2b771f110e186144a79fd70122857
Blake Griffith <cowlicks@riseup.net>
2017-04-23 19:45:24 -0700

    Refactor analysis script.

--- analysis.py
+++ analysis.py
@@ -2,44 +2,65 @@
 
 from collections import defaultdict
 import json
-from pprint import pprint as print
+import os
+from pprint import pprint
 import sys
 
-data_filename = sys.argv[1] or 'results.json'
-with open(data_filename) as f:
-    data = f.readlines()
-data = json.loads(''.join(data))
 
-action_counts = defaultdict(int)
-for k, v in data['action_map'].items():
-    action_counts[v['heuristicAction']] += 1
-print(action_counts)
+def load(data_location):
+    with open(data_location) as f:
+        data = f.readlines()
+    pprint("Loaded data from %s " % data_location)
+    return json.loads(''.join(data))
 
 
-snitch_counts = defaultdict(int)
-tracker_hosts = defaultdict(int)
-for k, v in data['snitch_map'].items():
-    snitch_counts[len(v)] += 1
-    for tracker in v:
-        tracker_hosts[tracker] += 1
+def analyze(data):
+    action_counts = defaultdict(int)
+    for k, v in data['action_map'].items():
+        action_counts[v['heuristicAction']] += 1
 
-tracker_host_count = defaultdict(int)
-for k, v in tracker_hosts.items():
-    tracker_host_count[v] += 1
+    snitch_counts = defaultdict(int)
+    tracker_hosts = defaultdict(int)
+    for k, v in data['snitch_map'].items():
+        snitch_counts[len(v)] += 1
+        for tracker in v:
+            tracker_hosts[tracker] += 1
 
-cookieblocks = [k for k, v in data['action_map'].items() if v['heuristicAction'] == 'cookieblock']
-blocks = [k for k, v in data['action_map'].items() if v['heuristicAction'] == 'block']
+    tracker_host_count = defaultdict(int)
+    for k, v in tracker_hosts.items():
+        tracker_host_count[v] += 1
 
-print('tracker host count')
-print(tracker_host_count)
-print('snitch_counts')
-print(snitch_counts)
-print('supercookie counts')
-print(len(data['supercookie_domains']))
-# fortune.com has 35 trackers!!
-# optimizely is seen on 29 sites!! 
+    cookieblocks = [k for k, v in data['action_map'].items() if v['heuristicAction'] == 'cookieblock']
+    blocks = [k for k, v in data['action_map'].items() if v['heuristicAction'] == 'block']
 
-print('supercookies with cookieblocks')
-print(len(set(data['supercookie_domains']) & set(cookieblocks)))
-print('supercookies with blocks')
-print(len(set(data['supercookie_domains']) & set(blocks)))
+    analysis_results = {
+            'action_counts': action_counts,
+            'tracker_host_count': tracker_host_count,
+            'snitch_counts': snitch_counts,
+            'supercookie_counts':  len(data['supercookie_domains']),
+            'supercookies_with_cookieblocks': len(set(data['supercookie_domains']) & set(cookieblocks)),
+            'supercookies_with_blocks': len(set(data['supercookie_domains']) & set(blocks)),
+            }
+    pprint("Data analyzed, here's what we got:")
+    pprint(analysis_results)
+    return analysis_results
+
+
+def save(out_location, data):
+    with open(out_location, 'w') as f:
+        f.write(json.dumps(data, indent=4, sort_keys=True))
+    pprint("Saved analysis to %s" % out_location)
+
+
+def main():
+    data_location = sys.argv[1] or 'results.json'
+    data = load(data_location)
+    results = analyze(data)
+
+    data_path, data_file = os.path.split(data_location)
+    out_location = os.path.join(data_path, 'analysis-' + data_file)
+    save(out_location, results)
+
+
+if __name__ == '__main__':
+    main()
